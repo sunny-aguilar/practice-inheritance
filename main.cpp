b2816a07947beb7c01754b450f8c294acc9c1994
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-12-11 20:49:01 -0800

    files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "Dog.hpp"
 #include <iostream>
 using std::endl;
+using std::cout;
 
 int main() {
     Dog moe;
-
+    moe.setName("Moemoe");
+    cout << moe.getName() << endl;
 
     return 0;
 }
